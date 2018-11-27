610e296950bec27048001386e2823d6717cc2775
Alessandra-Rossaro <s253112@studenti.polito.it>
2018-11-28 00:36:23 +0100

    MyAgent successor

--- MyAgent.py
+++ MyAgent.py
@@ -1,5 +1,7 @@
 from agent import AlphaBetaAgent
 import minimax
+from math import inf
+
 
 """
 Agent skeleton. Fill in the gaps.
@@ -10,27 +12,37 @@ class MyAgent(AlphaBetaAgent):
     """
   This is the skeleton of an agent to play the Tak game.
   """
+    # def iterative_deepening_search(self,state):
+    #     for depth in range(0, 10):
+    #         result = minimax.search(state, self)
+    #         #if result!=
 
     def get_action(self, state, last_action, time_left):
         self.last_action = last_action
         self.time_left = time_left
+        # qui aggiorna max depth
         return minimax.search(state, self)
 
-    """
-  The successors function must return (or yield) a list of
-  pairs (a, s) in which a is the action played to reach the
-  state s.
-  """
+
+
 
     def successors(self, state):
+        """The successors function must return (or yield) a list of
+        pairs (a, s) in which a is the action played to reach the
+        state s;"""
         actionsPlayer = state.get_current_player_actions()
+        playerId = state.get_cur_player()
         listPairs = []
         for action in actionsPlayer:
             if state.is_action_valid(action):
                 newState = state.copy()
                 newState.apply_action(action)
                 listPairs.append((action, newState))
-        return listPairs
+        # if player == max (= me) order of list is decreasing, otherwise is increasing
+        return sorted(listPairs, key=self.evaluate_successor, reverse=playerId == self.id)
+
+    def evaluate_successor(self, element):
+        return self.evaluate(element[1])
 
     """
   The cutoff function returns true if the alpha-beta/minimax
@@ -44,6 +56,8 @@ class MyAgent(AlphaBetaAgent):
             return True
         else:
             return False
+
+        # qui controlla se hai raggiunto max depth ( variabile di questa classe)
 
     """
   The evaluate function must return an integer value
